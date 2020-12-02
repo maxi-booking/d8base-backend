52ff371a548a8bef47aff3a8af92eddce3e1d4ea
webmalc <m@webmalc.pw>
2020-12-02 14:53:08 +0300

    Update the elasticsearch indexes config

--- services/documents.py
+++ services/documents.py
@@ -4,6 +4,7 @@ from django.db.models.query import QuerySet
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
+from professionals.models import Professional, ProfessionalTag
 from search.elasticsearch import languages_analyzer
 
 from .models import Service, ServiceTag
@@ -17,11 +18,23 @@ class ServiceDocument(Document):
         analyzer=languages_analyzer,
         fields={"raw": fields.KeywordField()},
     )
+    professional_name = fields.TextField(
+        analyzer=languages_analyzer,
+        fields={"raw": fields.KeywordField()},
+    )
     description = fields.TextField(
         analyzer=languages_analyzer,
         fields={"raw": fields.KeywordField()},
     )
+    professional_description = fields.TextField(
+        analyzer=languages_analyzer,
+        fields={"raw": fields.KeywordField()},
+    )
     tags = fields.TextField(
+        analyzer=languages_analyzer,
+        fields={"raw": fields.KeywordField()},
+    )
+    professional_tags = fields.TextField(
         analyzer=languages_analyzer,
         fields={"raw": fields.KeywordField()},
     )
@@ -34,6 +47,21 @@ class ServiceDocument(Document):
         """Return the tags as a string."""
         # pylint: disable=no-self-use
         return " ".join([t.name for t in instance.tags.all()])
+
+    def prepare_professional_name(self, instance: Service) -> str:
+        """Return the professional name."""
+        # pylint: disable=no-self-use
+        return instance.professional.name
+
+    def prepare_professional_description(self, instance: Service) -> str:
+        """Return the professional description."""
+        # pylint: disable=no-self-use
+        return instance.professional.description
+
+    def prepare_professional_tags(self, instance: Service) -> str:
+        """Return the tags as a string."""
+        # pylint: disable=no-self-use
+        return " ".join([t.name for t in instance.professional.tags.all()])
 
     def prepare_service_type(self, instance: Service) -> str:
         """Return the tags as a string."""
@@ -50,11 +78,12 @@ class ServiceDocument(Document):
         """The django settings class."""
 
         model = Service
-        related_models = [ServiceTag]
+        related_models = [ServiceTag, Professional, ProfessionalTag]
 
     def get_queryset(self) -> QuerySet:
         """Return the queryset."""
-        return super().get_queryset().prefetch_related("tags")
+        return super().get_queryset().select_related("professional").\
+            prefetch_related("tags")
 
     def get_instances_from_related(
         self,
@@ -62,4 +91,8 @@ class ServiceDocument(Document):
     ) -> Service:
         """Get a service from the tag."""
         # pylint: disable=no-self-use
+        if isinstance(related_instance, Professional):
+            return related_instance.services.all()
+        if isinstance(related_instance, ProfessionalTag):
+            return related_instance.professional.services.all()
         return related_instance.service
